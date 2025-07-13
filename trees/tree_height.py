790dabde8c99111e4a2433ce5c8b86327b483a4a
avi9800 <80779279+avi9800@users.noreply.github.com>
2025-07-14 01:42:57 +0530

    Create tree_height.py

---
+++ trees/tree_height.py
@@ -0,0 +1,47 @@
+# Tree Creation
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+def tree(arr):
+    # Create Node objects and store in list with dummy at index 0
+    nodes = [None]  # 0th index is unused
+    for val in arr:
+        nodes.append(Node(val))
+
+    # Link the nodes to form the binary tree
+    for i in range(1, len(nodes)):
+        if 2 * i < len(nodes):
+            nodes[i].left = nodes[2 * i]
+        if 2 * i + 1 < len(nodes):
+            nodes[i].right = nodes[2 * i + 1]
+
+    # The root of the tree is at index 1
+    root = nodes[1]
+    return root
+
+root = tree(arr) #Tree formed
+node = root
+
+from collections import deque
+
+def depth(node):
+    if node == None:
+        return 0
+    
+    lh = depth(node.left)
+    rh = depth(node.right)
+
+    return 1+max(lh, rh)
+        
+
+
+print(depth(root))
+    
+
+
+
